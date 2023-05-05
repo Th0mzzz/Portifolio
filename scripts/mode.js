07d5f1b26e95e6bb8a9a11e21d54401e9467430b
Thomaz <rm86681@estudante.fieb.edu.br>
2023-05-04 23:54:08 -0300

    .

--- scripts/mode.js
+++ scripts/mode.js
@@ -2,8 +2,3 @@ const mode = document.querySelectorAll(".modo");
 const moon = document.querySelector("#moon");
 const sun = document.querySelector("#sun");
 
-function mudarModo(){
-    mode.classListi
-
-
-}
