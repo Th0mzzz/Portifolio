41ca7fda03a3effaa7f253ce04b3178ec942d410
Thomaz <rm86681@estudante.fieb.edu.br>
2023-05-07 23:12:51 -0300

    .

--- scripts/header.js
+++ scripts/header.js
@@ -1,11 +1,2 @@
 
-let prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  let currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.querySelector("header").style.top = "0";
-  } else {
-    document.querySelector("header").style.top = "-20%";
-  }
-  prevScrollpos = currentScrollPos;
-}
+
