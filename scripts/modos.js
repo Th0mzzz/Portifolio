7814ed869bf432d58dc7089b40f32a3400b38869
Thomaz <122468659+Th0mzzz@users.noreply.github.com>
2023-05-08 20:35:42 +0000

    fdesas

--- scripts/modos.js
+++ scripts/modos.js
@@ -1,17 +1,40 @@
 const botaocolor = document.getElementById("toggle-btn");
 
-
 function mudarCor(){
-const fundoValue = document.documentElement.style.getPropertyValue("--cor-fundo");
+/*const fundoValue = document.documentElement.style.getPropertyValue("--cor-fundo");
+    
+Solução-1
 
 if(fundoValue === "#141414"){
-    document.documentElement.style.setProperty("--cor-primaria ", "#8C52FF");
-    document.documentElement.style.setProperty("--cor-secundaria ", "#000000");
-    document.documentElement.style.setProperty("--cor-fundo", "#F5F5DC")
+    document.documentElement.style.setProperty("--cor-primaria", "#8C52FF");
+    document.documentElement.style.setProperty("--cor-primaria-2", "#9662ff");
+    document.documentElement.style.setProperty("--cor-secundaria", "#000000");
 }else{
-    document.documentElement.style.setProperty("--cor-primaria ", "#87cefa");
-    document.documentElement.style.setProperty("--cor-secundaria ", "#ffffff");
-    document.documentElement.style.setProperty("--cor-fundo", "#141414")
+    document.documentElement.style.setProperty("--cor-primaria", "#87cefa");
+    document.documentElement.style.setProperty("--cor-secundaria", "#ffffff");
+    document.documentElement.style.setProperty("--cor-fundo", "#141414");
+}*/
+
+/*----Solução 2*/ 
+
+const root = document.querySelector(':root');
+
+if(root.classList == 'light'){
+    root.classList.remove('light')
+}else{
+    root.classList.add('light')
+}
+
+
+
+let emoji = document.getElementById('emoji');
+
+if( emoji == "&#x1F60E"){
+    emoji.innerHTML = "&#x1F634";
+
+}else{
+
+    emoji.innerHTML = "&#x1F60E";
 }
 
 
